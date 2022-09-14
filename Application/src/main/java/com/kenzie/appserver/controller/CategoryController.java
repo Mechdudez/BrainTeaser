2ccbf839cb1300179cffa49fcda8e1de511419c4
Renee Liu <reneesyliu@gmail.com>
2022-09-13 22:09:47 -0700

    implemented getQuestionByQuestionId endpoint in CategoryController

--- Application/src/main/java/com/kenzie/appserver/controller/CategoryController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.kenzie.appserver.controller;
 
 import com.kenzie.appserver.service.CategoryService;
 import com.kenzie.appserver.controller.model.CategoryResponse;
+import com.kenzie.appserver.service.model.Question;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,15 +18,21 @@ public class CategoryController {
     }
 
     @GetMapping("/{questionId}")
-    public ResponseEntity<CategoryResponse> getQuestionById(@PathVariable("questionId") int questionId) {
-        CategoryResponse question =
-                categoryService.findQuestionByID(questionId); //
+    public ResponseEntity<CategoryResponse> getQuestionById(@PathVariable("questionId") String questionId) {
+        Question question =
+                categoryService.getQuestionById(questionId); //
         // needs implementation
         if (question == null || question.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
 
-        return ResponseEntity.ok(question);
+        CategoryResponse categoryResponse = new CategoryResponse();
+        categoryResponse.setQuestionId(questionId);
+        categoryResponse.setQuestions(question.getQuestion());
+        categoryResponse.setAnswers(question.getAnswer());
+        categoryResponse.setDifficultyOfQuestion(question.getDiffLevel());
+
+        return ResponseEntity.ok(categoryResponse);
     }
 
 }
