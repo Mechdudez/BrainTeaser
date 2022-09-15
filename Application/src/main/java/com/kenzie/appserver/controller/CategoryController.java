f523b9944c88f11a0e62712035a28ed67f3c7549
Renee Liu <reneesyliu@gmail.com>
2022-09-14 21:52:12 -0700

    fixed categories/questionid endpoint

--- Application/src/main/java/com/kenzie/appserver/controller/CategoryController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/CategoryController.java
@@ -2,7 +2,7 @@ package com.kenzie.appserver.controller;
 
 import com.kenzie.appserver.service.CategoryService;
 import com.kenzie.appserver.controller.model.CategoryResponse;
-import com.kenzie.appserver.service.model.Question;
+import com.kenzie.appserver.service.model.Category;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,18 +19,18 @@ public class CategoryController {
 
     @GetMapping("/{questionId}")
     public ResponseEntity<CategoryResponse> getQuestionById(@PathVariable("questionId") String questionId) {
-        Question question =
+        Category category =
                 categoryService.getQuestionById(questionId); //
         // needs implementation
-        if (question == null || question.isEmpty()) {
+        if (category == null) {
             return ResponseEntity.noContent().build();
         }
 
         CategoryResponse categoryResponse = new CategoryResponse();
         categoryResponse.setQuestionId(questionId);
-        categoryResponse.setQuestions(question.getQuestion());
-        categoryResponse.setAnswers(question.getAnswer());
-        categoryResponse.setDifficultyOfQuestion(question.getDiffLevel());
+        categoryResponse.setQuestions(category.getQuestions());
+        categoryResponse.setAnswers(category.getAnswers());
+        categoryResponse.setDifficultyOfQuestion(category.getDifficultyOfAQuestion());
 
         return ResponseEntity.ok(categoryResponse);
     }
