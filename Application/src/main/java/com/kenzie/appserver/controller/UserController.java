c7e4212206abce397f14f165189ab6d9a4ecfae4
Renee Liu <reneesyliu@gmail.com>
2022-09-15 20:22:49 -0700

    UserController in the making

--- Application/src/main/java/com/kenzie/appserver/controller/UserController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/UserController.java
@@ -1,6 +1,20 @@
 package com.kenzie.appserver.controller;
 
+import com.kenzie.appserver.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private UserService userService;
+
+    UserController(UserService userService){
+        this.userService = userService;
+    }
+
+
 
 
 
--- Application/src/main/java/com/kenzie/appserver/service/UserService.java
+++ Application/src/main/java/com/kenzie/appserver/service/UserService.java
@@ -1,5 +1,8 @@
 package com.kenzie.appserver.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 
 
