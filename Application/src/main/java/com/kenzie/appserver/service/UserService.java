f855301a86fd45e1b7171cda71787006039e7c6f
Renee Liu <reneesyliu@gmail.com>
2022-09-15 22:26:02 -0700

    implemented getTopScore user in UserService, needs review

--- Application/src/main/java/com/kenzie/appserver/service/UserService.java
+++ Application/src/main/java/com/kenzie/appserver/service/UserService.java
@@ -1,20 +1,29 @@
 package com.kenzie.appserver.service;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
 import com.kenzie.appserver.controller.model.UserCreateRequest;
 import com.kenzie.appserver.repositories.CategoryRepository;
 import com.kenzie.appserver.repositories.UserRepository;
 import com.kenzie.appserver.repositories.model.CategoryRecord;
 import com.kenzie.appserver.repositories.model.UserRecord;
+import com.kenzie.appserver.service.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
+import java.util.Map.Entry;
 
 @Service
 public class UserService {
     private UserRepository userRepository;
+    private Dy
 
 
     public UserService(UserRepository userRepository) {
@@ -47,7 +56,34 @@ public class UserService {
 
     public UserRecord getUserWithTopScore(){
         // Need to think hard on this
+        DynamoDBMapperConfig mapperConfig = new DynamoDBMapperConfig.Builder()
+                .withTableNameOverride(DynamoDBMapperConfig.TableNameOverride.withTableNameReplacement("user"))
+                .build();
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression();
+        DynamoDBMapper mapper = new DynamoDBMapper(client, mapperConfig);
+        List<User> scanResult = mapper.scan(User.class,
+                scanExpression);
 
+
+        TreeMap<Integer, String> sortedPoints =
+                new TreeMap<>(Collections.reverseOrder());
+
+        for(User user:scanResult){
+            sortedPoints.put(user.getPoints(), user.getUserName());
+        }
+
+        Set<Entry<Integer, String>> mappings =
+                sortedPoints.entrySet();
+        List<UserRecord> sortedRecordByPoints = new ArrayList<>();
+        for(Entry<Integer, String> mapping : mappings) {
+            UserRecord topPointsUserRecord = new UserRecord();
+            topPointsUserRecord.setPoints(mapping.getKey());
+            topPointsUserRecord.setUsername(mapping.getValue());
+            sortedRecordByPoints.add(topPointsUserRecord);
+        }
+
+      return sortedRecordByPoints.get(0);
     }
 
 
