e28ffc97013e6c784098851eb3ee0125fc87a186
Renee Liu <reneesyliu@gmail.com>
2022-09-17 14:02:58 -0700

    formatting

--- Application/src/main/java/com/kenzie/appserver/controller/CategoryController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/CategoryController.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Category")
+@RequestMapping("/category")
 public class CategoryController {
 
     private CategoryService categoryService;
