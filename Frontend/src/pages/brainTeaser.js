eb7bf64aca988ce1cd64e4a15ef4a81cf8c616b1
Ryan Wiest <james.wiest@mykenzie.snhu.edu>
2022-10-09 21:43:29 -0700

    "Pushing changes for pipeline"

--- Frontend/src/pages/brainTeaser.js
+++ Frontend/src/pages/brainTeaser.js
@@ -79,7 +79,7 @@ class BrainTeaser extends BaseClass {
         let result = await this.client.getRandomQuestion(this.errorHandler);
 
 
-       await this.dataStore.set("question", result);
+       this.dataStore.set("question", result);
 
 
        if (result) {
